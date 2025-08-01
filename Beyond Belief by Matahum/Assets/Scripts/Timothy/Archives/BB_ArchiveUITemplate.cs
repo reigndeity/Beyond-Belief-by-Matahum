2399c334e35ca034103daa5c065a6771500a2c3d
RavenFraust12 <tjvangeles08@gmail.com>
2025-08-01 10:15:08 +0800

    Update 08/01/2025 - 10:14am
    - A bit of Archive Update
    - Added a button Manager for Quest Journal

---
+++ Beyond Belief by Matahum/Assets/Scripts/Timothy/Archives/BB_ArchiveUITemplate.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class BB_ArchiveUITemplate : MonoBehaviour
+{
+    public Image iconImage;
+    public TextMeshProUGUI nameText;
+    public BB_ArchiveSO linkedArchiveSO;
+
+    public Sprite undiscoveredSprite;
+    public void Setup(BB_ArchiveSO archiveSO, Sprite defaultUndiscoveredSprite)
+    {
+        linkedArchiveSO = archiveSO;
+        undiscoveredSprite = defaultUndiscoveredSprite;
+        Refresh();
+    }
+
+    public void Refresh()
+    {
+        if (linkedArchiveSO == null) return;
+
+        if (linkedArchiveSO.isDiscovered)
+        {
+            nameText.text = linkedArchiveSO.archiveName;
+            iconImage.sprite = linkedArchiveSO.archiveImage;
+        }
+        else
+        {
+            nameText.text = "?????";
+            iconImage.sprite = undiscoveredSprite;
+        }
+    }
+}
