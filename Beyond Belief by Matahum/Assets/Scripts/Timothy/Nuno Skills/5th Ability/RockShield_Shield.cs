173ed08a215eaf83451e1956499b5c983fede3c9
RavenFraust12 <tjvangeles08@gmail.com>
2025-08-28 22:36:10 +0800

    Update - 08/28/2025 - 10:35pm
    - Created Skill 5: Rock Shield
    - Not yet polished, since there are no nuno stat yet, only skills.

---
+++ Beyond Belief by Matahum/Assets/Scripts/Timothy/Nuno Skills/5th Ability/RockShield_Shield.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class RockShield_Shield : MonoBehaviour, IDamageable
+{
+    public float shieldHealth;
+    [HideInInspector] public RockShield_ShieldHolder shieldHolder;
+
+    public bool IsDead() => shieldHealth <= 0;
+
+    public void Init(RockShield_ShieldHolder holder, float health)
+    {
+        shieldHolder = holder;
+        shieldHealth = health;
+        gameObject.SetActive(true);
+    }
+
+    public void TakeDamage(float damage)
+    {
+        shieldHealth -= damage;
+
+        if (IsDead())
+        {
+            gameObject.SetActive(false);
+            shieldHealth = 0f;
+            shieldHolder.OnShieldDestroyed(this);
+        }
+    }
+}
