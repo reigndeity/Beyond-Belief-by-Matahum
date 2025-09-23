57d2adcb8ebebe99ce5abca0db9dd807982c06fe
Randall Jay Ramirez <158044567+reigndeity@users.noreply.github.com>
2025-09-23 12:07:55 +0800

    update 09/23/2025 - 12:07pm
    - fixed teleport saving

--- Beyond Belief by Matahum/Assets/Scripts/Rjay/Map/PersistantGuid.cs
+++ Beyond Belief by Matahum/Assets/Scripts/Rjay/Map/PersistantGuid.cs
@@ -7,19 +7,41 @@ public class PersistentGuid : MonoBehaviour
     [SerializeField, HideInInspector] private string guid;
     public string Guid => guid;
 
-    // Generate once and keep forever (unless you force-regenerate)
     private void OnValidate()
     {
         if (string.IsNullOrEmpty(guid))
-            guid = System.Guid.NewGuid().ToString("N");
+        {
+            GenerateNewGuid();
+        }
+        else
+        {
+#if UNITY_EDITOR
+            // Check for duplicates in the scene
+            var allGuids = UnityEngine.Object.FindObjectsOfType<PersistentGuid>();
+            foreach (var g in allGuids)
+            {
+                if (g != this && g.Guid == guid)
+                {
+                    // Duplicate detected → regenerate
+                    GenerateNewGuid();
+                    break;
+                }
+            }
+#endif
+        }
     }
 
-    [ContextMenu("Regenerate GUID (Use Carefully)")]
-    private void Regenerate()
+    private void GenerateNewGuid()
     {
         guid = System.Guid.NewGuid().ToString("N");
 #if UNITY_EDITOR
         UnityEditor.EditorUtility.SetDirty(this);
 #endif
+    }
+
+    [ContextMenu("Regenerate GUID (Use Carefully)")]
+    private void Regenerate()
+    {
+        GenerateNewGuid();
     }
 }
