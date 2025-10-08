0293dbedbbbe72944f9c1f87e0a53a7788970440
Randall Jay Ramirez <158044567+reigndeity@users.noreply.github.com>
2025-10-08 19:01:27 +0800

    update 10/08/2025 - 7:00pm
    - updated nuno cutscene timeline

--- Beyond Belief by Matahum/Assets/Scripts/Rjay/TimelineUtilities.cs
+++ Beyond Belief by Matahum/Assets/Scripts/Rjay/TimelineUtilities.cs
@@ -2,8 +2,16 @@ using UnityEngine;
 
 public class TimelineUtilities : MonoBehaviour
 {
+
+    public AudioClip cutsceneAudio;
+    public AudioSource cutsceneAudioSource;
     public void LoadNextScene(int index)
     {
         Loader.Load(index);
+    }
+
+    public void PlayCutsceneAudio()
+    {
+        cutsceneAudioSource.PlayOneShot(cutsceneAudio);
     }
 }
