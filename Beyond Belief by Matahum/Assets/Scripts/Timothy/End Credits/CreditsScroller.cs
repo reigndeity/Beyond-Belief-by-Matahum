e38ae7ed8b1a9ff51fe9808391c99c87b846eec2
RavenFraust12 <tjvangeles08@gmail.com>
2025-10-08 22:47:19 +0800

    Update 10/08/2025 - 10:46pm
    - fixed added something sa end credits

--- Beyond Belief by Matahum/Assets/Scripts/Timothy/End Credits/CreditsScroller.cs
+++ Beyond Belief by Matahum/Assets/Scripts/Timothy/End Credits/CreditsScroller.cs
@@ -46,9 +46,9 @@ public class CreditsScroller : MonoBehaviour
     IEnumerator BackToMenu()
     {
         if (canvasGroup != null)
-            canvasGroup.FadeIn(0.5f);
+            canvasGroup.FadeIn(4.5f);
 
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(5f);
 
         UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenuScene");
     }
