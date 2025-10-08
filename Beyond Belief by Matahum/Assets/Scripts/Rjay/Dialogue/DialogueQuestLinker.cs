152a7f1e1d0f31d159ae7f1d5e46de9d3fed02f0
Randall Jay Ramirez <158044567+reigndeity@users.noreply.github.com>
2025-10-08 13:00:51 +0800

    update 10/08/2025 - 12:59pm
    - updated mangkukulam's eye
    - connected timothy's end credits

--- Beyond Belief by Matahum/Assets/Scripts/Rjay/Dialogue/DialogueQuestLinker.cs
+++ Beyond Belief by Matahum/Assets/Scripts/Rjay/Dialogue/DialogueQuestLinker.cs
@@ -805,11 +805,10 @@ public class DialogueQuestLinker : MonoBehaviour
         m_player.SetPlayerLocked(true);
         m_player.ForceIdleOverride();
         PlayerCamera.Instance.HardLockCamera();
-        await Task.Delay(1000);
         StartCoroutine(UI_TransitionController.instance.Fade(0f, 1f, 0.5f));
-        await Task.Delay(500);
-        await GameManager.instance.SavePlayerCoreData();
-        await Task.Delay(500);
+        await Task.Delay(1000);
+        await GameManager.instance.SaveAll();
+        await Task.Delay(1500);
         Loader.Load(19);
     }
     public void QuestPropertiesCheck()
